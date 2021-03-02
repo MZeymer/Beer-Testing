ba947f08c9a600d0c4e581f576e7cb1b3eb3c209
MZeymer <62883265+MZeymer@users.noreply.github.com>
2021-03-02 15:39:10 +0100

    Add project files.

---
+++ Beer Testing/Beer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Beer_Testing
+{
+    public class Beer
+    {
+        private int _id;
+        private string _name;
+        private double _price;
+        private double _abv;
+
+        public Beer(int id, string name, double price, double abv)
+        {
+            this._id = id;
+            this._name = name;
+            this._price = price;
+            this._abv = abv;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value == null) { throw new ArgumentNullException();}
+                if (value.Length < 4) { throw new ArgumentException();}
+
+                _name = value;
+            }
+        }
+
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                if (value <= 0) { throw new ArgumentException();}
+
+                _price = value;
+            }
+        }
+
+        public double Abv
+        {
+            get => _abv;
+            set
+            {
+                if (value < 0 || value > 100) throw new ArgumentOutOfRangeException();
+
+                _abv = value;
+            }
+        }
+    }
+}
---
+++ Beer TestingTests/BeerTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Beer_Testing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beer_Testing.Tests
+{
+    [TestClass()]
+    public class BeerTests
+    {
+        private Beer beer;
+
+        [TestInitialize]
+        public void Init()
+        {
+            beer = new Beer(1, "Carlsberg", 25, 4.7);
+        }
+
+
+        [TestMethod()]
+        public void NameTest()
+        {
+            Assert.AreEqual("Carlsberg", beer.Name);
+            Assert.ThrowsException<ArgumentException>(() => beer.Name = "Car");
+            Assert.ThrowsException<ArgumentNullException>(() => beer.Name = null);
+            
+        }
+
+        [TestMethod()]
+        public void PriceTest()
+        {
+            Assert.AreEqual(25, beer.Price);
+            Assert.ThrowsException<ArgumentException>(() => beer.Price = 0);
+        }
+
+        [TestMethod()]
+        public void AbvTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => beer.Abv = -1);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => beer.Abv = 110);
+            beer.Abv = 5;
+            Assert.AreEqual(5, beer.Abv);
+            beer.Abv = 100;
+            Assert.AreEqual(100, beer.Abv);
+            
+        }
+    }
+}
